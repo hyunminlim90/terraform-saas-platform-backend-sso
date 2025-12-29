9cbb8026ab6094836ad0ad2ce10c7aa0f2d542f8
mole <hyunmin.lim.90@icloud.com>
2025-12-29 22:40:32 +0900

    인스턴스 모듈 작성(리액트)

---
+++ note.rb
@@ -0,0 +1,13 @@
+=begin
+
+
+git init
+git config --local user.name "mole"
+git config --local user.email "hyunmin.lim.90@icloud.com"
+git switch --create feature-2025
+git remote add origin http://localhost:8080/opentofu/opentofu-backend/java-sso.git
+git add ./; git commit -m "인스턴스 모듈 작성(리액트)"; git push origin feature-2025
+
+
+
+=end
