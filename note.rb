72fc2eeb59aade874fc4d6bfa38b39ad61c6a2ea
mole <hyunmin.lim.90@icloud.com>
2026-02-07 21:19:22 +0900

    GITLAB 마이그레이션 2026

--- note.rb
+++ note.rb
@@ -2,7 +2,14 @@
 
 
 
+git init;
+git config --local user.name "mole";
+git config --local user.email "hyunmin.lim.90@icloud.com";
+git switch --create feature-2025;
+git remote add origin http://gitlab.opentofu.click/seed-opentofu/backend/java-sso.git;
+git add ./; git commit -m "GITLAB 마이그레이션 2026"; git push origin feature-2025;
 
+git remote set-url origin http://gitlab.opentofu.click/seed-opentofu/backend/java-sso.git
 
 
 
